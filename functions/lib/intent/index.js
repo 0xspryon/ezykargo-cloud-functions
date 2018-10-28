817663d3b40bd7fe16ab0e1dd1fcbbd7bdb8c38a
Yonga Springfield <springfieldyonga@outlook.com>
2018-10-28 21:29:21 +0100

    corrected bug on addtruckintent where there was an undefined issue.

--- functions/lib/intent/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Freightages_1 = require("./Freightages");
-exports.FreightagesIntent = Freightages_1.FreightagesIntent;
-const Trucks_1 = require("./Trucks");
-exports.TrucksIntent = Trucks_1.TrucksIntent;
-const Auth_1 = require("./Auth");
-exports.Auth = Auth_1.Auth;
-//# sourceMappingURL=index.js.map
--- functions/lib/models/intents/DeleteMomoProvider.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=DeleteMomoProvider.js.map
