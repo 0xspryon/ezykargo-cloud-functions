bb9fd3a604d0c20f14e8a515a6fafeff62b7bc9f
kevmax <wambakevin7@gmail.com>
2019-03-06 08:02:27 +0100

    finish first verion of make deposit ithout confirmation

--- functions/src/api/index.ts
+++ functions/src/api/index.ts
@@ -1,51 +1,48 @@
-
-import * as functions from 'firebase-functions';
-import * as express from 'express';
+import * as functions from "firebase-functions";
+import * as express from "express";
 import * as bodyParser from "body-parser";
-import { TransactionsIntent } from '../intent';
+import { TransactionsIntent } from "../intent";
 
 const app = express();
 const main = express();
 // const axios = require('axios');
 
-export const cloud_function_url_notification = "https://us-central1-ezykargo-61790.cloudfunctions.net/api/v1/sE8BFAAE5EEABCA864224363759A55351B0DAA792C03.php"
-export const baseUrl = "https://api.monetbil.com/payment/v1/"
-export const placePayment = baseUrl+"placePayment"
-export const checkPayment = baseUrl+"checkPayment"
-export const serviceKey = "MkiGgmqZ0btHaB0rv7WbqiYv5WB3m4jy"
-export const serviceSecret = "lfO7ARbbMH4PNB6sBKkfYZv4F8ZudIo2JlgKZRWUwcBHpjJPyyokGn7MpRRvi2ys"
+export const cloud_function_url_notification =
+  "https://us-central1-ezykargo-61790.cloudfunctions.net/api/v1/sE8BFAAE5EEABCA864224363759A55351B0DAA792C03.php";
+export const baseUrl = "https://api.monetbil.com/payment/v1/";
+export const placePayment = baseUrl + "placePayment";
+export const checkPayment = baseUrl + "checkPayment";
+export const serviceKey = "MkiGgmqZ0btHaB0rv7WbqiYv5WB3m4jy";
+export const serviceSecret =
+  "lfO7ARbbMH4PNB6sBKkfYZv4F8ZudIo2JlgKZRWUwcBHpjJPyyokGn7MpRRvi2ys";
 
-const validIp= [
-    "184.154.224.14",
-    "184.154.224.222"
-]
+const validIp = ["184.154.224.14", "184.154.224.222"];
 
-main.use('/api/v1', app);
+main.use("/api/v1", app);
 main.use(bodyParser.json());
 main.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/E8BFAAE5EEABCA864224363759A55351B0DAA792C03.php', (req, res) => {
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    if(validIp.indexOf(`${ip}`)===-1){
-        res.send('error');
-        return;
-    }
-    const data = req.body
-    TransactionsIntent.validatePayment(data)
-        .then(()=>{
-            res.send('success');
-        }).catch(()=>{
-            res.send('error');
-        })
-})
+app.post("/sE8BFAAE5EEABCA864224363759A55351B0DAA792C03.php", (req, res) => {
+  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  if (validIp.indexOf(`${ip}`) === -1) {
+    res.send("error");
+    return;
+  }
+  const data = req.body;
+  TransactionsIntent.validatePayment(data)
+    .then(() => {
+      res.send("success");
+    })
+    .catch(() => {
+      res.send("error");
+    });
+});
 
 const webApi = functions.https.onRequest(main);
 export default webApi;
 
-
-
-        //MoMo
-        /*{
+//MoMo
+/*{
             "status": REQUEST_ACCEPTED,
             "message": payment pending,
             "channel_ussd": *126#,
@@ -53,8 +50,8 @@ export default webApi;
             "channel": CM_MTNMOBILEMONEY,
             "paymentId": 17759286369594791363
             }*/
-        //OM
-        /*{
+//OM
+/*{
             "status": REQUEST_ACCEPTED,
             "message": payment pending,
             "channel_ussd": #150*4*4#,
@@ -63,7 +60,7 @@ export default webApi;
             "payment_url": https://api.monetbil.com/orangemoney/v1/93514644691982916222/payment,
             "paymentId": 93514644691982916222
         }*/
-        /*Channel used to make payment.
+/*Channel used to make payment.
          CM_MTNMOBILEMONEY
          CM_ORANGEMONEY
          CM_EUMM
